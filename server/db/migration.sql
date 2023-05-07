5435d87cb12d47ca36c786e08f88b2bdef6b9fb3
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-06 22:16:15 -0700

    migration.sql created

---
+++ server/db/migration.sql
@@ -0,0 +1,60 @@
+DROP TABLE IF EXISTS usr CASCADE;
+DROP TABLE IF EXISTS brief;
+DROP TABLE IF EXISTS dont;
+DROP TABLE IF EXISTS brief_dont;
+DROP TABLE IF EXISTS fave;
+
+CREATE TABLE usr (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL,
+    email VARCHAR(25) NOT NULL,
+    password VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE brief (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(25) NOT NULL,
+    usr_id INT,
+    CONSTRAINT fk_usr
+        FOREIGN KEY(usr_id)
+        REFERENCES usr(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE dont (
+    id SERIAL PRIMARY KEY,
+    cat VARCHAR(25) NOT NULL,
+    descr VARCHAR(200) NOT NULL,
+    brief_id INT,
+    CONSTRAINT fk_brief
+        FOREIGN KEY(brief_id)
+        REFERENCES brief(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE brief_dont (
+    brief_id INT,
+    dont_id INT,
+    CONSTRAINT fk_brief
+        FOREIGN KEY(brief_id)
+        REFERENCES brief(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_dont
+        FOREIGN KEY(dont_id)
+        REFERENCES dont(id)
+        ON DELETE CASCADE    
+);
+
+CREATE TABLE fave (
+    usr_id INT,
+    dont_id INT,
+    PRIMARY KEY (usr_id, dont_id),
+    CONSTRAINT fk_usr
+        FOREIGN KEY(usr_id)
+        REFERENCES usr(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_dont
+        FOREIGN KEY(dont_id)
+        REFERENCES dont(id)
+        ON DELETE CASCADE
+);
