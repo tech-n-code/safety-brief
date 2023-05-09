2dbd3c582cd5ce68f799dbaa0935f91a015f5465
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-09 02:18:55 -0700

    minor edits

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -3,7 +3,7 @@ import BriefCard from "./BriefCard";
 
 function Dashboard(props) {
     const [briefsData, setBriefsData] = useState([]);
-    const placeholder = 1; //props.usrID
+    const placeholder = 3; //props.usrID
     console.log(briefsData)
 
     useEffect(() => {
@@ -25,8 +25,8 @@ function Dashboard(props) {
     
     return (
         <div className="container mx-auto">
-            <h1 className="text-3xl font-bold mb-6">My Safety Briefs</h1>
-            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            <h1 className="text-xl font-bold text-info mx-6 my-3">Collection</h1>
+            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-6">
                 {briefsData.length > 0 && briefsData.map((briefData) => (
                     <BriefCard
                         key={briefData.id}
