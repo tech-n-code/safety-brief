c08efcb1f1e3a24995b80d453e2efd2d28478c1b
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-09 04:13:34 -0700

    added NewBrief form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
 import './App.css'
 import Dashboard from './Dashboard';
+import NewBrief from './NewBrief'
 
 function App() {
+    const [showNewBriefForm, setShowNewBriefForm] = useState(false);
+    
+    const handleNewBriefClick = () => {
+        setShowNewBriefForm(true);
+    };
 
     return (
         <>
-            <Navigation/>
-            <Dashboard/>
+            <Navigation handleNewBriefClick={handleNewBriefClick} />
+            {showNewBriefForm && <NewBrief />}
+            <Dashboard />
         </>
     );
 }
 
-function Navigation() {
+function Navigation(props) {
 
     return (
-        <div className="navbar flex bg-base-300 rounded-box">
+        <div className="navbar flex bg-base-300 fixed top-0 w-full z-50">
             <div className="justify-end px-2">
                 <a className="text-3xl font-bold">Safety Brief!</a>
             </div> 
             <div className="flex justify-end flex-1 px-2">
-                <DropDown/>
+                <DropDown handleNewBriefClick={props.handleNewBriefClick} />
             </div>
         </div>
     );
@@ -36,12 +43,13 @@ function Avatar() {
     );
 }
 
-function DropDown() {
+function DropDown(props) {
+
     return (
         <div className="dropdown dropdown-end">
             <Avatar />
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>New Brief</a></li>
+                <li onClick={props.handleNewBriefClick}><a>New Brief</a></li>
                 <li><a>Logout</a></li>
             </ul>
         </div>
