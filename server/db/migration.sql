0e14345cf6ae03cb90702f9963acb3e55f453fb2
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-09 10:57:54 -0700

    painful renaming from `dont` to `cue`

--- server/db/migration.sql
+++ server/db/migration.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS usr CASCADE;
 DROP TABLE IF EXISTS brief CASCADE;
-DROP TABLE IF EXISTS dont;
-DROP TABLE IF EXISTS brief_dont;
+DROP TABLE IF EXISTS cue;
+DROP TABLE IF EXISTS brief_cue;
 DROP TABLE IF EXISTS fave;
 
 CREATE TABLE usr (
@@ -14,7 +14,7 @@ CREATE TABLE usr (
 
 CREATE TABLE brief (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(25) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     usr_id INT,
     CONSTRAINT fk_usr
         FOREIGN KEY(usr_id)
@@ -22,37 +22,37 @@ CREATE TABLE brief (
         ON DELETE CASCADE
 );
 
-CREATE TABLE dont (
+CREATE TABLE cue (
     id SERIAL PRIMARY KEY,
     cat VARCHAR(25) NOT NULL,
     descr VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE brief_dont (
+CREATE TABLE brief_cue (
     brief_id INT,
-    dont_id INT,
+    cue_id INT,
     checked BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (brief_id, dont_id),
+    PRIMARY KEY (brief_id, cue_id),
     CONSTRAINT fk_brief
         FOREIGN KEY(brief_id)
         REFERENCES brief(id)
         ON DELETE CASCADE,
-    CONSTRAINT fk_dont
-        FOREIGN KEY(dont_id)
-        REFERENCES dont(id)
+    CONSTRAINT fk_cue
+        FOREIGN KEY(cue_id)
+        REFERENCES cue(id)
         ON DELETE CASCADE    
 );
 
 CREATE TABLE fave (
     usr_id INT,
-    dont_id INT,
-    PRIMARY KEY (usr_id, dont_id),
+    cue_id INT,
+    PRIMARY KEY (usr_id, cue_id),
     CONSTRAINT fk_usr
         FOREIGN KEY(usr_id)
         REFERENCES usr(id)
         ON DELETE CASCADE,
-    CONSTRAINT fk_dont
-        FOREIGN KEY(dont_id)
-        REFERENCES dont(id)
+    CONSTRAINT fk_cue
+        FOREIGN KEY(cue_id)
+        REFERENCES cue(id)
         ON DELETE CASCADE
 );
