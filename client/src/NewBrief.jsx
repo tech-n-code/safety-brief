1da5245b5f575f41ef2091a0966a48d47c1acd9e
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-05-09 10:59:55 -0700

    fixed API calls to add cues to brief

--- client/src/NewBrief.jsx
+++ client/src/NewBrief.jsx
@@ -6,10 +6,11 @@ function NewBrief(props) {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('/api/categories')
+        fetch("http://localhost:3000/api/safety-brief/cue/cat")
             .then(response => response.json())
             .then(data => {
             console.log(data); // categories data
+            setCategories(data);
             })
             .catch(error => {
             console.error(error);
@@ -20,35 +21,67 @@ function NewBrief(props) {
         setCategoryAmounts({ ...categoryAmounts, [category]: parseInt(e.target.value) });
     };
 
-    const handleCreateBrief = async (title, user_id) => {
-        const response = await fetch('/api/safety-brief/briefs', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ title, user_id })
-        })
-        if (!response.ok) {
-            console.log('Failed to add favorite');
+    const handleCreateBrief = async () => {
+        try {
+            // Create the brief
+            const data = { title: briefTitle, usr_id: props.authUserID };
+            const response = await fetch('http://localhost:3000/api/safety-brief/brief', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            if (!response.ok) {
+                console.log('Failed to create BRIEF');
+            }
+            const brief = await response.json();
+            const briefId = brief[0].id;
+    
+            // Add cues to the brief for each category
+            if (briefId !== null) {
+                for (const category of categories) {
+                    const numCues = categoryAmounts[category.cat];
+                    if (numCues > 0) {
+                        const cueResponse = await fetch('http://localhost:3000/api/safety-brief/brief_cue', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ brief_id: briefId, cat: category.cat, num_cues: numCues })
+                        });
+                        if (!cueResponse.ok) {
+                            console.log(`Failed to add cues for category ${category.cat}`);
+                            continue;
+                        }
+                    }
+                }
+            }
+                
+            console.log('Brief and cues created successfully!');
+            // props.addBrief(brief[0]); // Add new brief to the briefs state
+            // props.hideForm(); // Hide the NewBrief component
+        } catch (error) {
+            console.error(error);
         }
     };
 
-  return (
-    <div className="container mx-6 pt-20">
+    return (
+        <div className="container mx-6 pt-20">
 
-        <label className="text-xl font-bold" htmlFor="brief-title">Brief Title:</label>
-        <input className="input input-primary w-full max-w-xs mx-3" type="text" placeholder="Type here" id="brief-title" value={briefTitle} onChange={e => setBriefTitle(e.target.value)} />
+            <label className="text-xl font-bold" htmlFor="brief-title">Brief Title:</label>
+            <input className="input input-primary w-full max-w-xs mx-3" type="text" placeholder="Type here" id="brief-title" value={briefTitle} onChange={e => setBriefTitle(e.target.value)} />
 
-        {categories.map(category => (
-            <div key={category.id}>
-                <label htmlFor={`category-${category.id}`}>{category.name}:</label>
-                <input type="number" id={`category-${category.id}`} value={categoryAmounts[category.name] || 0} onChange={e => handleInputChange(e, category.name)} />
-            </div>
-        ))}
+            {categories.map((category, index) => (
+                <div key={index}>
+                    <label htmlFor={`category-${category.id}`}>{`${category.cat} (${category.qty})`}:</label>
+                    <input type="number" id={`category-${category.id}`} value={categoryAmounts[category.cat] || 0} onChange={e => handleInputChange(e, category.cat)} />
+                </div>
+            ))}
 
-      <button onClick={handleCreateBrief}>Create Brief</button>
-    </div>
-  );
+        <button onClick={handleCreateBrief}>Create Brief</button>
+        </div>
+    );
 }
 
 export default NewBrief;
